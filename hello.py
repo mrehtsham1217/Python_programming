9c0ae4e9d70e8232979cedb089407d5d97cdb43a
MR_EHTSHAM <125168180+mrehtsham1217@users.noreply.github.com>
2023-02-12 07:55:12 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,58 @@
+# # print("Hello world")
+# #Tyecasting in python
+# # x = 10
+# # salary = float(x)
+# # #program to check type of any varivbble
+# # print(type(salary))
+# # name = input("Input your name:\t")
+# # print("You entered:\t",name)
+
+# # print(type(name))
+# # del name
+
+# # num1 = input("Enter first number:\t")
+# # num1 = int(num1)
+# # num2 = input("Enter second number:\t")
+# # num2 = int(num2)
+# # print("Sum of two numbers",num1+num2)
+# # print("Subtraction of two numbers",num1-num2)
+# # print("Multiplicaton of two numbers",num1*num2)
+# # print("Divisio of two numbers",num1/num2)
+
+# name = "Ehtsham khaliq"
+# print("COmplex string\t", name)
+# print("Index string\t", name[0])
+# print("Index string\t", name[-5])
+# print("slicing string\t", name[0:8])
+
+# name = "   Ehtsham khaliq son of abdul khaliq lives in layyah    "
+# print(name.strip())
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.replace("layyah", "Chowk Azam"))
+# print("z" in name)
+
+
+# first = "Ehtsham"
+# last = "Khaliq"
+# firstLast = first + "\t" + last
+# print(firstLast)
+
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = "I want {itemno} pieces of item {quantity} for {price} dollars."
+# # print(myorder.format(itemno, quantity, price))
+# import turtle
+# colors = ['red','purple','blue','green','orange','yellow']
+# t = turtle.pen()
+# turtle.bgcolor('black')
+# for i in range(360):
+#     t.pencolor(colors[i%6])
+#     t.width(i/100+1)
+#     t.farward(i)
+#     t.left(59)
+#     turtle.exitonclick()
+import math
+print(round(math.sqrt(10)))
