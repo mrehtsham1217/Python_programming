a5eaf71795c465a20b53b7e8d5a0131588039f81
MR_EHTSHAM <125168180+mrehtsham1217@users.noreply.github.com>
2023-02-16 12:04:53 +0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,25 @@
+name = "string in python string python javascript"
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.title())  # Capitilize each letter of word
+# print(name.rstrip("!"))
+# print(name.replace("pYthOn", "Javascript"))
+# print(name.split(" "))
+# print(len(name))
+# print(len(name))
+# print(len(name.center(100)))
+# print(name.count("python"))
+# print(name.endswith("javascript"))
+# print(name.find("javascript"))
+# print(name.index("python"))
+# print(name.isalnum())
+
+
+# name = "   Ehtsham khaliq son of abdul khaliq lives in layyah    "
+# print(name.strip())
+# print(len(name))
+# print(name.upper())
+# print(name.lower())
+# print(name.replace("layyah", "Chowk Azam"))
+# print("z" in name)
